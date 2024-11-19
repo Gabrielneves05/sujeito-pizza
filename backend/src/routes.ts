eda772eeef1c237dc904f76af4a8e4f844d5eea0
Gabrielneves05 <gabrielneves05@outlook.com>
2024-11-18 22:35:11 -0300

    feat: creation of functionalities for closing orders / adding items to the order / removing items from a specific table

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -12,6 +12,10 @@ import { CreateProductController } from './controllers/product/CreateProductCont
 import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 
 import { CreateOrderController } from './controllers/order/CreateOrderController';
+import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+
+import { AddItemController } from './controllers/order/AddItemController';
+import { RemoveItemController } from './controllers/order/RemoveItemController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
@@ -23,22 +27,23 @@ const upload = multer(uploadConfig.upload('./tmp'));
 
 // Routers user
 router.post('/users', new CreateUserController().handle);
-
 router.post('/login', new AuthUserController().handle);
-
 router.get('/me', isAuthenticated, new DetailUserController().handle);
 
 // Routers category
 router.post('/category', isAuthenticated , new CreateCategoryController().handle);
-
 router.get('/category', isAuthenticated, new ListCategoryController().handle);
 
 // Routers product
 router.post('/product', isAuthenticated, upload.single('file') ,new CreateProductController().handle);
-
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle);
 
 // Routers order
 router.post('/order', isAuthenticated, new CreateOrderController().handle);
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
+
+// Routers order item
+router.post('/order/add', isAuthenticated, new AddItemController().handle);
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle);
 
 export { router };
